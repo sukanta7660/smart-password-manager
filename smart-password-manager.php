b697bbf25071a8cde6cd334112c6651baf24add1
Sukanta Purkayastha <spurkayastha83@gmail.com>
2023-05-03 12:18:04 +0600

    Database tables are created

--- smart-password-manager.php
+++ smart-password-manager.php
@@ -37,10 +37,15 @@ class SmartPasswordManager {
     add_action('init', array($this, 'custom_post_type'));
   }
 
+  function register(): void
+  {
+      add_action('admin_enqueue_scripts', array( $this, 'enqueue' ));
+  }
+
   function activate(): void
   {
-    // generate Custom post type
-    $this->custom_post_type();
+    // tables Create
+    $this->createTables();
 
     flush_rewrite_rules();
   }
@@ -55,16 +60,63 @@ class SmartPasswordManager {
     register_post_type('folder', ['public' => true, 'label' => 'Folders']);
   }
 
+  function createTables(): void
+  {
+      global $wpdb;
+      $folderTableName = $wpdb->prefix . 'folders';
+      $credentialTableName = $wpdb->prefix . 'credentials';
+      $charsetCollate = $wpdb->get_charset_collate();
+
+      $folderTable = "CREATE TABLE IF NOT EXISTS $folderTableName(
+                `id` int NOT NULL AUTO_INCREMENT,
+                `user_id` int NOT NULL,
+                `name` varchar(255),
+                PRIMARY KEY (`id`)
+        ) $charsetCollate";
+
+      $credentialTable = "CREATE TABLE IF NOT EXISTS $credentialTableName(
+                `id` int NOT NULL AUTO_INCREMENT,
+                `user_id` int NOT NULL,
+                `folder_id` int,
+                `item_type` ENUM('login', 'card', 'identity') DEFAULT 'login',
+                `name` varchar(255) NOT NULL,
+                `username` varchar(255),
+                `password` varchar(255),
+                url varchar(255),
+                notes LONGTEXT,
+                PRIMARY KEY (`id`)
+        ) $charsetCollate";
+
+      require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+      try {
+
+          dbDelta( $folderTable );
+          dbDelta( $credentialTable );
+          error_log('Tables are created');
+
+      } catch (Exception $exception) {
+          error_log('Something Went Wrong');
+      }
+  }
+
+  function enqueue(): void
+  {
+      wp_enqueue_style('myPluginStyle', plugins_url('/assets/css/style.css', __FILE__));
+      wp_enqueue_script('myPluginScript', plugins_url('/assets/js/main.js', __FILE__));
+  }
+
 }
 
 if ( class_exists('SmartPasswordManager') ) {
 
   $smartPasswordManager = new SmartPasswordManager();
+  $smartPasswordManager->register();
 
 }
 
 // Activation
-register_activation_hook(__FILE__, array( $smartPasswordManager, 'deactivate'));
+register_activation_hook(__FILE__, array( $smartPasswordManager, 'activate'));
 
 // Deactivation
-register_deactivation_hook(__FILE__, array( $smartPasswordManager, 'activate'));
+register_deactivation_hook(__FILE__, array( $smartPasswordManager, 'deactivate'));
