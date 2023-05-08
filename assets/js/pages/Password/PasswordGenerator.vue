dfea049373dcaaa9d582dd1b0fba96d3e973b4f8
Sukanta Purkayastha <spurkayastha83@gmail.com>
2023-05-08 16:58:51 +0600

    transition added

--- assets/js/pages/Password/PasswordGenerator.vue
+++ assets/js/pages/Password/PasswordGenerator.vue
@@ -32,117 +32,119 @@
                             </el-radio-group>
                         </el-col>
 
-                        <el-col
-                            v-if="state.passwordType === 'password'"
-                            :span="24"
-                            class="mt-4"
-                        >
-                            <el-row :gutter="20">
-                                <el-col :span="8">
-                                    <el-form-item label="Length">
-                                        <el-input
-                                            type="number"
-                                            v-model.number="state.passwordGenerator.length"
-                                            placeholder="Length"
+                        <el-collapse-transition>
+                            <el-col
+                                v-if="state.passwordType === 'password'"
+                                :span="24"
+                                class="mt-4"
+                            >
+                                <el-row :gutter="20">
+                                    <el-col :span="8">
+                                        <el-form-item label="Length">
+                                            <el-input
+                                                type="number"
+                                                v-model.number="state.passwordGenerator.length"
+                                                placeholder="Length"
+                                            />
+                                        </el-form-item>
+                                    </el-col>
+
+                                    <el-col :span="8">
+                                        <el-form-item label="Minimum Number">
+                                            <el-input
+                                                type="number"
+                                                v-model.number="state.passwordGenerator.minimumNumber"
+                                                placeholder="Minimum number"
+                                            />
+                                        </el-form-item>
+                                    </el-col>
+
+                                    <el-col :span="8">
+                                        <el-form-item label="Minimum Special Char">
+                                            <el-input
+                                                type="number"
+                                                v-model.number="state.passwordGenerator.minimumSpecialCharacter"
+                                                placeholder="Minimum special character"
+                                            />
+                                        </el-form-item>
+                                    </el-col>
+                                </el-row>
+
+                                <el-row :gutter="20">
+                                    <b class="mb-4">Options</b>
+                                    <el-col :span="24">
+                                        <el-checkbox
+                                            v-model="state.passwordGenerator.options.uppercase"
+                                            :label="passwordOptions.uppercase.label"
+                                            :value="passwordOptions.uppercase.value"
+                                            size="large"
+                                        /> <br>
+                                        <el-checkbox
+                                            v-model="state.passwordGenerator.options.numbers"
+                                            :label="passwordOptions.numbers.label"
+                                            :value="passwordOptions.numbers.value"
+                                            size="large"
+                                        /> <br>
+                                        <el-checkbox
+                                            v-model="state.passwordGenerator.options.special"
+                                            :label="passwordOptions.special.label"
+                                            :value="passwordOptions.special.value"
+                                            size="large"
+                                        /> <br>
+                                        <el-checkbox
+                                            v-model="state.passwordGenerator.options.ambiguous"
+                                            :label="passwordOptions.ambiguous.label"
+                                            :value="passwordOptions.ambiguous.value"
+                                            size="large"
                                         />
-                                    </el-form-item>
-                                </el-col>
+                                    </el-col>
+                                </el-row>
+                            </el-col>
 
-                                <el-col :span="8">
-                                    <el-form-item label="Minimum Number">
-                                        <el-input
-                                            type="number"
-                                            v-model.number="state.passwordGenerator.minimumNumber"
-                                            placeholder="Minimum number"
-                                        />
-                                    </el-form-item>
-                                </el-col>
+                            <el-col
+                                v-else
+                                :span="24"
+                                class="mt-4"
+                            >
+                                <el-row :gutter="20">
+                                    <el-col :span="12">
+                                        <el-form-item label="Number of Words">
+                                            <el-input
+                                                type="text"
+                                                v-model.number="state.passPhraseGenerator.numberOfWords"
+                                                placeholder="Number of Words"
+                                            />
+                                        </el-form-item>
+                                    </el-col>
 
-                                <el-col :span="8">
-                                    <el-form-item label="Minimum Special Char">
-                                        <el-input
-                                            type="number"
-                                            v-model.number="state.passwordGenerator.minimumSpecialCharacter"
-                                            placeholder="Minimum special character"
-                                        />
-                                    </el-form-item>
-                                </el-col>
-                            </el-row>
+                                    <el-col :span="12">
+                                        <el-form-item label="Separator">
+                                            <el-input
+                                                type="text"
+                                                v-model="state.passPhraseGenerator.wordSeparator"
+                                                placeholder="Separator"
+                                            />
+                                        </el-form-item>
+                                    </el-col>
+                                </el-row>
 
-                            <el-row :gutter="20">
-                                <b class="mb-4">Options</b>
-                                <el-col :span="24">
-                                    <el-checkbox
-                                        v-model="state.passwordGenerator.options.uppercase"
-                                        :label="passwordOptions.uppercase.label"
-                                        :value="passwordOptions.uppercase.value"
-                                        size="large"
-                                    /> <br>
-                                    <el-checkbox
-                                        v-model="state.passwordGenerator.options.numbers"
-                                        :label="passwordOptions.numbers.label"
-                                        :value="passwordOptions.numbers.value"
-                                        size="large"
-                                    /> <br>
-                                    <el-checkbox
-                                        v-model="state.passwordGenerator.options.special"
-                                        :label="passwordOptions.special.label"
-                                        :value="passwordOptions.special.value"
-                                        size="large"
-                                    /> <br>
-                                    <el-checkbox
-                                        v-model="state.passwordGenerator.options.ambiguous"
-                                        :label="passwordOptions.ambiguous.label"
-                                        :value="passwordOptions.ambiguous.value"
-                                        size="large"
-                                    />
-                                </el-col>
-                            </el-row>
-                        </el-col>
-
-                        <el-col
-                            v-else
-                            :span="24"
-                            class="mt-4"
-                        >
-                            <el-row :gutter="20">
-                                <el-col :span="12">
-                                    <el-form-item label="Number of Words">
-                                        <el-input
-                                            type="text"
-                                            v-model.number="state.passPhraseGenerator.numberOfWords"
-                                            placeholder="Number of Words"
-                                        />
-                                    </el-form-item>
-                                </el-col>
-
-                                <el-col :span="12">
-                                    <el-form-item label="Separator">
-                                        <el-input
-                                            type="text"
-                                            v-model="state.passPhraseGenerator.wordSeparator"
-                                            placeholder="Separator"
-                                        />
-                                    </el-form-item>
-                                </el-col>
-                            </el-row>
-
-                            <el-row :gutter="20">
-                                <b class="mb-4">Options</b>
-                                <el-col :span="24">
-                                    <el-checkbox
-                                        v-model="state.passPhraseGenerator.capitalize"
-                                        label="Capitalize"
-                                        size="large"
-                                    /> <br>
-                                    <el-checkbox
-                                        v-model="state.passPhraseGenerator.isIncludeNumber"
-                                        label="Include Number"
-                                        size="large"
-                                    /> <br>
-                                </el-col>
-                            </el-row>
-                        </el-col>
+                                <el-row :gutter="20">
+                                    <b class="mb-4">Options</b>
+                                    <el-col :span="24">
+                                        <el-checkbox
+                                            v-model="state.passPhraseGenerator.capitalize"
+                                            label="Capitalize"
+                                            size="large"
+                                        /> <br>
+                                        <el-checkbox
+                                            v-model="state.passPhraseGenerator.isIncludeNumber"
+                                            label="Include Number"
+                                            size="large"
+                                        /> <br>
+                                    </el-col>
+                                </el-row>
+                            </el-col>
+                        </el-collapse-transition>
                     </el-row>
                 </el-card>
             </el-col>
